0c87934c455a2fd520966b96233b34348bcf8abd
Olivier Groulx <o.groulx@gmail.com>
2022-07-09 19:05:14 -0400

    Split generator update and points update.
    Also restored the single rgb line generator.

--- js/generator.js
+++ js/generator.js
@@ -43,17 +43,57 @@ function GenerateRamp(numStepsRGB, numStepsExp, startExp, rangeExp)
     return output;
 }
 
-/*
-if(params.colorIndex == -1 || counter == params.colorIndex)
-{
-    positions.push( fp.x, fp.y, fp.z );
 
-    if(counter == params.colorIndex)
+function GetRatio(numStepsRGB, index)
+{
+    var counter = 0;
+    for ( let k = 0; k < 3; k ++ )
     {
-        params.ratio_r = ratio.x;
-        params.ratio_g = ratio.y;
-        params.ratio_b = ratio.z;
+        for ( let a = 0; a <= numStepsRGB; a ++ )
+        {
+            for ( let b = 0; b <= numStepsRGB; b ++ )
+            {
+                if(index == counter)
+                {
+                    var ratio = new THREE.Vector3();
+
+                    if(k == 0)
+                    {
+                        ratio.x = 1;
+                        ratio.y = a / numStepsRGB;
+                        ratio.z = b / numStepsRGB;
+                    }
+                    else if(k == 1)
+                    {
+                        ratio.x = a / numStepsRGB;
+                        ratio.y = 1;
+                        ratio.z = b / numStepsRGB;
+                    }
+                    else if(k == 2)
+                    {
+                        ratio.x = a / numStepsRGB;
+                        ratio.y = b / numStepsRGB;
+                        ratio.z = 1;
+                    }
+                    return ratio;
+                }
+                counter ++;
+            }
+        }
     }
 }
 
-*/
+function GenerateLine(RGBRatio, numStepsExp, startExp, rangeExp)
+{
+    var output = [];
+    for ( let i = startExp; i <= startExp + rangeExp; i ++ )
+    {
+        for(let j = 0; j <= numStepsExp; j++)
+        {
+            var ex = Math.pow(2, i + (j / numStepsExp));
+            var p = RGBRatio.clone().multiplyScalar(ex);
+            output.push(p);
+        }
+    }
+    return output;
+}
